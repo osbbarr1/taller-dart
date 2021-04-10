67ab3619c1c2446533cfa3ee5e6ec1a82319689d
osbbarr1 <osbbarr1@gmail.com>
2021-04-10 15:33:42 -0500

    ejercicio de clases  usando Herencias

--- 6.clases/clases.dart
+++ 6.clases/clases.dart
@@ -8,6 +8,18 @@ class Persona{
  // Persona(this.nombre, this.apellido, this.edad);
   
   Persona({this.nombre, this.apellido, this.edad});
+  
+  obtenerNombre()  => this.nombre;
+}
+
+class Empleado extends Persona{
+  String puesto;
+  Empleado(this.puesto , ) : super(nombre : "oscar ", apellido: "Baquero", edad : 38);
+}
+
+class Vehiculo{
+  int? puertas;
+  String? color;
 }
 
 void main(){
@@ -15,4 +27,7 @@ void main(){
   
     var persona =  Persona(nombre: "Oscar", apellido: "Baquero",edad: 38);
   print(persona);
+  
+  var empleado = Empleado("cajero");
+  print(empleado.obtenerNombre());
 }
