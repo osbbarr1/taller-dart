a15182094b4410c96284fccbabb98b3644e03340
osbbarr1 <osbbarr1@gmail.com>
2021-04-10 15:26:13 -0500

    ejercicio de clases  usando name param

--- 6.clases/clases.dart
+++ 6.clases/clases.dart
@@ -1,16 +1,18 @@
 
 class Persona{
-  String nombre;
-  String apellido;
-  int edad;
+  String? nombre;
+  String? apellido;
+  int? edad;
 
 //constructor dart
-  Persona(this.nombre, this.apellido, this.edad);
+ // Persona(this.nombre, this.apellido, this.edad);
+  
+  Persona({this.nombre, this.apellido, this.edad});
 }
 
 void main(){
 //  var persona = new Persona("Oscar", "Baquero", 38);
   
-    var persona =  Persona("Oscar", "Baquero", 38);
+    var persona =  Persona(nombre: "Oscar", apellido: "Baquero",edad: 38);
   print(persona);
 }
