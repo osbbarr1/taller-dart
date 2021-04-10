24aa8ed80cafd0d03435b54bbd298359e5826a00
osbbarr1 <osbbarr1@gmail.com>
2021-04-10 14:24:40 -0500

    ejercicio de funciones con parametro requerido

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -2,11 +2,12 @@ void main() {
 
 var resultado1 = obtenerNombre("Oscar");
 var resultado2 = obtenerNombre2("Oscar");
-var resultado3 = obtenerNombre3(valor : "Oscar andres");
+var resultado3 = obtenerNombre3(valor : "Oscar andres", edad : null);
+var resultado4 = obtenerNombre4(valor : "Oscar andres", edad : 38);
 print(resultado1);
 print(resultado2);
 print(resultado3);
-
+print(resultado4);
 }
 
  String obtenerNombre(String valor){
@@ -21,4 +22,11 @@ print(resultado3);
     String obtenerNombre3({String valor = "Oscar Baquero", int?  edad}){
 
     return 'mi nombre es $valor';
+  }
+
+  
+
+ String obtenerNombre4({String valor = "Oscar Baquero", required int  edad}){
+
+    return 'mi nombre es $valor y mi edad es $edad';
   }
