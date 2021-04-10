dc0e1c595a8fe28b6c0638741162bee9758e3b5e
osbbarr1 <osbbarr1@gmail.com>
2021-04-10 16:21:26 -0500

    ejercicio de clases usando mixin

--- 6.clases/clases.dart
+++ 6.clases/clases.dart
@@ -12,9 +12,33 @@ class Persona{
   obtenerNombre()  => this.nombre;
 }
 
-class Empleado extends Persona{
-  String puesto;
+class Empleado extends Persona with Aeropuerto{
+  String? puesto;
   Empleado(this.puesto , ) : super(nombre : "oscar ", apellido: "Baquero", edad : 38);
+
+  Empleado.fromJson(this.puesto);
+}
+
+class Trabajador implements Empleado{
+  
+  String? puesto;
+  String? nombre;
+  String? apellido;
+  int? edad;
+  int? salas;
+  obtenerNombre() {
+    throw UnimplementedError();
+  }
+  
+  nombreAeropuerto(String valor){
+    return "valor nombre aeropuerto en trabajador";
+    
+  }
+}
+
+mixin Aeropuerto{
+  int? salas;
+  nombreAeropuerto(String aeropuerto) => "El nombre aero puerto es ==>  $aeropuerto";
 }
 
 class Vehiculo{
@@ -29,5 +53,6 @@ void main(){
   print(persona);
   
   var empleado = Empleado("cajero");
+  print(empleado.nombreAeropuerto("Aero puerto el dorado"));
   print(empleado.obtenerNombre());
 }
