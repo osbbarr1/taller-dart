2003f0405c743b0dfd76506b9b78161f674ee06a
osbbarr1 <osbbarr1@gmail.com>
2021-04-10 15:13:26 -0500

    ejercicio de Excepciones usando try & catch

--- excepciones/excepciones.dart
+++ excepciones/excepciones.dart
@@ -1,8 +1,22 @@
 void main(){
-  print(obtenerNombre("Oscarr"));
+  try{
+  print(obtenerNombre("Oscar"));   
+  }catch(e){
+    print("hubo un error, contacte con el administrador ");
+  }
+ 
 }
 
 String obtenerNombre (String? nombre){
+    if(nombre =='Oscar'){
+      throw FormatException("No puede enviar $nombre");
+    }
+
+    return "mi nombre es $nombre";
+  }
+
+
+String obtenerNombre2 (String? nombre){
     if(nombre =='Oscar'){
       throw FormatException("No puede enviar $nombre");
     }
