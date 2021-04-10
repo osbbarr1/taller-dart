aa4ee0dc5287606528f2c28efed87ced7da10f66
osbbarr1 <osbbarr1@gmail.com>
2021-04-10 11:14:02 -0500

    ejercicio de variables

---
+++ 1. variables/variable.dart
@@ -0,0 +1,14 @@
+void main(){
+  var nombre = "Oscar";
+  print(nombre);
+  var edad = 38;
+  print(edad);
+  String nombres= "Oscar Baquero";
+  print(edad);
+  int telefono = 351654316;
+  print(telefono);
+  String telefonos = "65431654";
+print(telefonos);
+  var apellido;
+  print(apellido);
+}
